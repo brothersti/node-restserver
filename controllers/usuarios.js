1e078f20b017247027d0b4483935acc26eb09040
Brothers It <hilardy56@yahoo.fr>
2024-06-06 11:43:50 -0600

    Primer commint

---
+++ controllers/usuarios.js
@@ -0,0 +1,47 @@
+const { response } = require('express')
+
+
+const usuariosGet = (req, res = response) => {
+    const query = req.query
+
+    res.json({
+        msg: 'get API - controlador'
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+    const body = req.body
+
+    res.json({
+        msg: 'post API - controlador',
+        body
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+    const { id } = req.params
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
